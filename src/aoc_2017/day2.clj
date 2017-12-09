66488abd4111e9a171c3271bd963fbe82ba54283
Stéphane Tavera <stephane.tavera@gmail.com>
2017-12-09 12:43:02 +0100

    day2

--- src/aoc_2017/day2.clj
+++ src/aoc_2017/day2.clj
@@ -4,6 +4,26 @@
   [line]
   (- (apply max line) (apply min line)))
 
+(defn cs-divide
+  [[a b]]
+  (when (zero? (mod b a))
+    (/ b a)))
+
+(defn cs-divide-line
+  [line]
+  (let [r (range (count line))
+        pairs (for [i r j r :when (not= i j)]
+                (sort [(nth line i) (nth line j)]))]
+    (some cs-divide pairs)))
+
 (defn cs
+  [f lines]
+  (reduce #(+ %1 (f %2)) 0 lines))
+
+(defn sol1
   [lines]
-  (reduce #(+ %1 (cs-line %2)) 0 lines))
+  (cs cs-line lines))
+
+(defn sol2
+  [lines]
+  (cs cs-divide-line lines))
