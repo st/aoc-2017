3ffcb7c698ada1e4953cf89c59b0c6e426e276b5
Stéphane Tavera <stephane.tavera@gmail.com>
2017-12-10 14:55:26 +0100

    day 6

--- src/aoc_2017/day6.clj
+++ src/aoc_2017/day6.clj
@@ -1,1 +1,36 @@
 (ns aoc-2017.day6)
+
+(defn first-max
+  [bank]
+  (->> bank
+       (map-indexed (fn [idx itm] [idx itm]))
+       (sort-by (juxt second (comp - first)))
+       last))
+
+(defn distribute
+  [bank i v]
+  (if (zero? v)
+    bank
+    (let [insertion (mod (inc i) (count bank))]
+      (recur (update bank insertion inc)
+             insertion
+             (dec v)))))
+
+(defn redistribute
+  [bank]
+  (let [[i v] (first-max bank)]
+    (-> bank
+        (assoc i 0)
+        (distribute i v))))
+
+(defn redistribute-until-seen
+  ([exit-fn bank]
+   (redistribute-until-seen exit-fn bank {} 1))
+  ([exit-fn bank seen gen]
+   (let [new-bank (redistribute bank)]
+     (if (seen new-bank)
+       (exit-fn gen seen new-bank)
+       (recur exit-fn new-bank (assoc seen new-bank gen) (inc gen))))))
+
+(def gen-size (partial redistribute-until-seen (fn [gen _ _] gen)))
+(def loop-size (partial redistribute-until-seen (fn [gen seen bank] (- gen (seen bank)))))
--- test/aoc_2017/test_day6.clj
+++ test/aoc_2017/test_day6.clj
@@ -3,4 +3,13 @@
     [clojure.test :refer :all]
     [aoc-2017.day6 :refer :all]))
 
+(deftest test-sol
+  (is (= 5 (gen-size
+                [0 2 7 0])))
+  (is (= 4 (loop-size
+                [0 2 7 0])))
+  (is (= 3156 (gen-size
+                [2 8 8 5 4 2 3 1 5 5 1 2 15 13 5 14])))
+  (is (= 1610 (loop-size
+                [2 8 8 5 4 2 3 1 5 5 1 2 15 13 5 14]))))
 
