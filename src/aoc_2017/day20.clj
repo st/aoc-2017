c1ee9562340a02482f4b9d79388791ed54317053
Stéphane Tavera <stephane.tavera@gmail.com>
2018-01-03 17:24:28 +0100

    day 20 2nd part beurk

--- src/aoc_2017/day20.clj
+++ src/aoc_2017/day20.clj
@@ -1,24 +1,47 @@
 (ns aoc-2017.day20
   (:require
-    [clojure.string :as s]))
+    [clojure.string :as s]
+    [clojure.set :as set]))
+
+(defn before
+  [s c]
+  (let [end (.lastIndexOf s c)]
+    (subs s 0 end)))
+
+(defn parse-numbers
+  [s]
+  (map read-string (s/split (before s ">") #",")))
+
+(defn parse-particule
+  [line]
+  (let [[_ p v a] (s/split line #"<")]
+    (map parse-numbers [p v a])))
 
 (defn parse-acc
   [line]
-  (let [start (.lastIndexOf line "<")
-        end (.lastIndexOf line ">")
-        raw-acc (subs line (inc start) end)]
-    (map read-string (s/split raw-acc #","))))
+  (let [[_ _ a] (parse-particule line)]
+    a))
+
+(defn add-coordinates
+  [as bs]
+  (map + as bs))
+
+(defn sub-coordinates
+  [as bs]
+  (map - as bs))
 
 (defn distance
-  [[x y z]]
-  (reduce + (map #(Math/abs %) [x y z])))
+  ([p]
+   (reduce + (map #(* % %) p)))
+  ([p0 p1]
+   (distance (sub-coordinates p0 p1))))
 
 (defn indexed-lines
   [input]
   (->> (s/split (slurp input) #"\n")
        (map-indexed (fn [i e] [i e]))))
 
-(defn score
+(defn distance-to-origin
   [[i line]]
   (distance (parse-acc line)))
 
@@ -26,5 +49,33 @@
   [input]
   (->> input
        indexed-lines
-       (sort-by score)
+       (sort-by distance-to-origin)
        ffirst))
+
+(defn next-pos
+  [[p v a]]
+  (let [new-v (add-coordinates v a)]
+    [(add-coordinates p new-v) new-v a]))
+
+(defn remove-collisions
+  [ps]
+  (->> ps
+       (group-by first)
+       (filter (fn [[pos ps]] (= 1 (count ps))))
+       vals
+       (map first)))
+
+(defn remove-collisions-next
+  [ps]
+  (->> ps
+       remove-collisions
+       (map next-pos)))
+
+(defn sol2
+  ;; beurk
+  [input]
+  (->> input
+       indexed-lines
+       (map (fn [[_ l]] (parse-particule l)))
+       (iterate remove-collisions-next)))
+
--- test/aoc_2017/test_day20.clj
+++ test/aoc_2017/test_day20.clj
@@ -5,4 +5,6 @@
 
 (deftest test-parse
   (is (= [3 -4 0] (parse-acc "p=<-4524,4899,1314>, v=<69,-62,-36>, a=<3,-4,0>")))
-  (is (= 157 (sol "src/resources/day20.txt"))))
+  (is (= 157 (sol "src/resources/day20.txt")))
+
+  (is (= 499 (count (nth (sol2 "src/resources/day20.txt") 1000)))))
